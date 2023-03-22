c4194e33950d74d8a1ac4fcaedfcfec241a4c16a
sadasivuni durga <durgasadasivuni23@gmail.com>
2023-03-22 18:38:46 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,17 +1,17 @@
 import openai
 import time
-
-openai.api_key = "YOUR_API_KEY_HERE"
-
+​
+openai.api_key = "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"
+​
 # Define the prompt
-prompt = ("some prompt ")
-
+prompt = ("an unexpected visitor ")
+​
 # Set up the OpenAI API parameters
 model_engine = "davinci"
 temperature = 0.7
 max_tokens = 512
 stop_sequence = "\n\n"
-
+​
 # Generate the story
 response = openai.Completion.create(
     engine=model_engine,
@@ -20,9 +20,10 @@ response = openai.Completion.create(
     max_tokens=max_tokens,
     stop=stop_sequence
 )
-
+​
 # Extract the generated story from the response
 generated_text = response.choices[0].text.strip()
-
+​
 # Print the generated story
 print(generated_text)
+​
